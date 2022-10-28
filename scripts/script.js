f45c9c43d6b378abfb99dcfa3043a95156fef35f
alanadiastech <ads.pict@gmail.com>
2022-10-28 17:12:10 -0300

    Search

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+// JavaScript code
+function pesquisar() {
+	let input = document.getElementById('searchbar').value
+	input=input.toLowerCase();
+	let x = document.getElementsByClassName('pesquisa');
+	
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display="none";
+		}
+		else {
+			x[i].style.display="list-item";				
+		}
+	}
+}
+
+
+function clearList(){
+    while (pesquisa.firstChild){
+        pesquisa.removeChild(pesquisa.firstChild)
+    }
+}
